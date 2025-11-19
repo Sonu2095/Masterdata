3e4639bdc70d3d0886f6cb02c2afa8def9d7e408
AreebAl <areebcalm@gmail.com>
2025-11-19 16:58:44 +0530

    /ResreveExtension api

---
+++ src/main/java/com/avaya/amsp/masterdata/dtos/ReserveExtensionRequestDto.java
@@ -0,0 +1,31 @@
+package com.avaya.amsp.masterdata.dtos;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReserveExtensionRequestDto {
+    
+    @NotNull(message = "callNumber is required")
+    @Valid
+    private CallNumber callNumber;
+    
+    private Integer maxReservationTimeInSecs;
+    
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CallNumber {
+        @NotNull(message = "areacode is required")
+        private Long areacode;
+        
+        @NotNull(message = "extension is required")
+        private String extension;
+    }
+}
+
